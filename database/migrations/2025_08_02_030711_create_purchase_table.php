ad6d1df008d1b599612de32462a0f9a6cbc7ff66
aldair-meneses <aldair.meneses@protonmail.com>
2025-08-02 00:48:53 -0300

    "feat: add purchase table migration for managing user purchases"

---
+++ database/migrations/2025_08_02_030711_create_purchase_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner')->constrained('users')->onDelete('cascade');
+            $table->char('purchase_name', 255);
+            $table->boolean('installment_purchase');
+            $table->integer('installments')->nullable();
+            $table->integer('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
